eaf4bd2cb069714e99971cc0f0b84323d33ee96b
Shinra DX <shinra.dx@outlook.com>
2022-05-20 21:57:38 +0800

    [SERVER] 定义邮箱验证码创建函数

--- server/app/common.php
+++ server/app/common.php
@@ -1,2 +1,20 @@
 <?php
-// 应用公共文件
+declare (strict_types = 1);
+
+/**
+ *  清城舞萌助手 后端接口应用 - 公共函数库
+ *
+ *  @author 3.80GHz (X-buster) <shinra.dx@outlook.com>
+ *  @copyright 2022 3.80GHz (X-buster)
+ *  @license MIT
+ */
+
+/**
+ *  创建邮箱验证码
+ * 
+ *  @return string 长度6位的验证码
+ */
+
+function generateEmailHash(): string {
+    return strtoupper(substr(md5(uniqid((string) rand(), true)), 0, 6));
+}
