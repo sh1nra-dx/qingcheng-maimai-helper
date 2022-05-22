22d420bd51c52c2fc7934976b05d7dc310c5d0d2
Shinra DX <shinra.dx@outlook.com>
2022-05-22 20:35:02 +0800

    [DB] 修改资讯内容和分类的表间关系

--- database/info_system.sql
+++ database/info_system.sql
@@ -9,7 +9,6 @@
 DROP TABLE IF EXISTS mai_info;
 CREATE TABLE mai_info (
     post_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    category_id INT UNSIGNED NOT NULL,
     post_title VARCHAR(200) NOT NULL,
     post_content TEXT DEFAULT NULL,
     post_draft_flag TINYINT(1) UNSIGNED NOT NULL DEFAULT 1,
@@ -27,4 +26,14 @@ CREATE TABLE mai_info_category (
     category_update_time DATETIME NOT NULL,
     category_delete_time INT DEFAULT NULL,
     PRIMARY KEY (category_id)
+) ENGINE = InnoDB DEFAULT CHARSET = 'utf8';
+
+DROP TABLE IF EXISTS mai_relation_info_category;
+CREATE TABLE mai_relation_info_category (
+    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    post_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (post_id),
+    INDEX (category_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = 'utf8';
